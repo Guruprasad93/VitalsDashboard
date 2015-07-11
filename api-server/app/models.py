bbec037d1ae773081a692daff226944796b21edc
Guruprasad <gpr.1993@gmail.com>
2015-07-12 02:40:44 +0400

    Bootstrap Dashboard

---
+++ api-server/app/models.py
@@ -0,0 +1,154 @@
+
+from flask import request, current_app
+from app import db
+import datetime
+import json
+
+
+class Serializer(object):
+  __public__ = None
+  "Must be implemented by implementors"
+
+  def to_serializable_dict(self):
+    dict = {}
+    for public_key in self.__public__:
+      value = getattr(self, public_key)
+      if value:
+        dict[public_key] = value
+    return dict
+
+class SWEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, Serializer):
+      return obj.to_serializable_dict()
+    if isinstance(obj, (datetime.date)):
+      return obj.isoformat()
+    return json.JSONEncoder.default(self, obj)
+
+
+def SWJsonify(*args, **kwargs):
+  return json.dumps(dict(*args, **kwargs), cls=SWEncoder, indent=None if request.is_xhr else 2)
+  # stolen from https://github.com/mitsuhiko/flask/blob/master/flask/helpers.py
+
+
+
+class User(db.Model, Serializer):
+    __public__ = ['id','nickname','email']
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    devices = db.relationship('Device', backref='user', lazy='dynamic')
+    vitals = db.relationship('Vital', backref='user', lazy='dynamic')
+    sos = db.relationship('SOS', backref='user', lazy='dynamic')   
+    notifications = db.relationship('Notification', backref='user', lazy='dynamic')
+ 
+    # def __repr__(self):
+    #     return '<User %r>' % (self.nickname)
+
+    def __init__(self, nickname, email):
+        self.nickname = nickname
+        self.email = email
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
+
+    def __init__(self, body, user_id):
+        self.body = body
+        self.timestamp = datetime.datetime.now()
+        self.user_id = user_id
+
+
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), index=True, unique=True)
+    type = db.Column(db.String(25))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  
+    def __repr__(self):
+        return '<Device %r>'% (self.name)
+
+    def __init__(self, name, type, user_id):
+        self.name = name
+        self.type = type
+        self.user_id = user_id
+
+class Vital(db.Model, Serializer):
+    __public__ = ['id','timestamp','user_id','tempinternal','tempexternal','heartrate','bloodoxy','baro']
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    tempinternal = db.Column(db.Float(precision=4), default=0)
+    tempexternal = db.Column(db.Float(precision=4), default=0)
+    heartrate = db.Column(db.Float(precision=4), default=0)
+    bloodoxy = db.Column(db.Float(precision=4), default=0)
+    baro = db.Column(db.Float(precision=4), default=0)
+    
+    def __init__(self, user_id, tempinternal, tempexternal, heartrate, bloodoxy, baro):
+        self.timestamp = datetime.datetime.now()
+        self.user_id = user_id
+        self.tempinternal = tempinternal
+        self.tempexternal = tempexternal
+        self.heartrate = heartrate
+        self.bloodoxy = bloodoxy
+        self.baro = baro
+
+
+class SOS(db.Model, Serializer):
+    __public__ = ['id','timestamp','user_id','sostxt']
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    sostxt = db.Column(db.String(200))
+    
+    # def __repr__(self):
+    #     return '<SOS %r>' % (self.user_id)
+
+    def __init__(self, user_id, sostxt):
+        self.timestamp = datetime.datetime.now()
+        self.user_id = user_id
+        self.sostxt = sostxt
+
+
+   
+class Notification(db.Model, Serializer):
+    __public__ = ['id','timestamp','user_id','msg','mode']
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    msg = db.Column(db.Text)
+    mode = db.Column(db.String(10))
+    
+    # def __repr__(self):
+    #     return '<Notification %r>' % (self.user_id)
+     
+
+    def __init__(self, user_id, msg, mode):
+        self.timestamp = datetime.datetime.now()
+        self.user_id = user_id
+        self.msg = msg
+        self.mode = mode
+
+
+  
+class NotificationContact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime)
+    email = db.Column(db.String(25))
+    phonenum = db.Column(db.String(25))
+    
+    def __repr__(self):
+        return '<NotificationContact %r>' % (self.email)
+     
+    def __init__(self, email, phonenum):
+        self.timestamp = datetime.datetime.now()
+        self.email = email
+        self.phonenum = phonenum
+
---
+++ vitals.sh
@@ -0,0 +1,75 @@
+#! usr/bin/bash
+
+# Seeding Random Generator
+RANDOM=$$$(date +%s)
+
+
+# List of Device id's
+#declare -a all_Connected_devices=(dev_001 dev_002 dev_003 dev_004 dev_005)
+declare -a all_Connected_devices=(1 2 3 4 5)
+num_Connect_devices=${#all_Connected_devices[@]} 
+num_Connect_devices=$((num_Connect_devices-1))
+echo $num_Connect_devices
+
+# Initializing counter
+counter=0
+ctr=0
+# infinite while loop
+while true
+do 
+	echo $counter
+	device_id=${all_Connected_devices[$counter]}
+	
+	#Generating Health Vitals
+	#HR=$(gshuf -i 60-100 -n 1)
+	HR=$(python -S -c "import random; print random.randint(60,100)")
+	echo "HeartRate="$HR
+	#Blood_oxygen=$(shuf -i 90-99 -n 0.01)
+	#echo "Blood oxygen=",$Blood_oxygen
+
+	Blood_oxygen=$(python -S -c "import random; print random.uniform(90,99)")
+	echo "Blood oxygen="$Blood_oxygen
+
+	body_temp=$(python -S -c "import random; print random.uniform(95,103)")
+	echo "Body temperature="$body_temp
+
+	env_temp=$(python -S -c "import random; print random.uniform(55,85)")
+	echo "Environment Temperature="$env_temp
+
+	#Generating position (altitude) of fireman
+	position=$(python -S -c "import random; print random.uniform(50,5000)")
+	echo "Position="$position
+
+	if [ "$counter" -eq $num_Connect_devices ];
+	then 
+		echo "Reset counter to zero"
+		counter=0
+	else
+		echo "Increment counter"
+		counter=$((counter+1))
+	fi
+
+#       if [$counter -eq num_Connect_devices-1]
+#	then 
+#		echo "Counter has reached max"
+#		counter=0
+#	else
+#		echo "Ïncrement counter"
+#	fi
+
+	echo "All values to be passed to API=" $HR, $Blood_oxygen, $body_temp, $env_temp, $position
+
+	#API CAll (Dummy)
+	#curl --request GET echo.jsontest.com/heartrate/$HR/Blood_O2/$Blood_oxygen
+	curl -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/api/$device_id/vitals -d '{"HR": "'"$HR"'", "Blood Oxygen": "'"$Blood_oxygen"'", "Body_Temp":"'"$body_temp"'", "Env_temp": "'"$env_temp"'", "Barometer_read":"'"$position"'"}'
+	
+	ctr=$((ctr+1))
+	echo $ctr
+	if [ $((ctr%10)) -eq 0 ]; then
+		echo "SOS warning"
+		curl -H "Content-Type: text/plain" -X POST http://127.0.0.1:5000/api/$device_id/SOS -d '{I am in danger}'
+		exit 0
+	fi
+
+
+done
